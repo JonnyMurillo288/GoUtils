3943aa637e3ab64ca1ccb8889a1fc3790c741ade
JonnyMurillo288 <39134582+JonnyMurillo288@users.noreply.github.com>
2021-07-07 11:34:47 -0700

    Add files via upload

--- allCombo.go
+++ allCombo.go
@@ -1,36 +1,5 @@
 package utils
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-func AllCombo(arr []int) [][]int {
-
-	var all [][]int
-	poss := Factorial(len(arr))
-	for i := 0; i < poss; i++ {
-		rand.Seed(int64(time.Now().Nanosecond()))
-		j := rand.Intn(len(arr))
-		l := rand.Intn(len(arr))
-		new := Switch(arr,j,l)
-		fmt.Println("all:",all)
-		fmt.Println("new:",new)
-		fmt.Println("arr",arr)
-		fmt.Println("\n",i)
-		if !InIntArrArr(all,new) {
-			all = append(all,new)
-			fmt.Println("all appended:",all)
-			time.Sleep(time.Second*5)
-			arr = Switch(arr,j,l)
-			continue
-		}
-		i--
-	}
-	return all
-}
-
 
 func Permutations(arr []int)[][]int{
     var helper func([]int, int)
@@ -60,10 +29,6 @@ func Permutations(arr []int)[][]int{
     return res
 }
 
-func Switch(arr []int, j int, l int) []int {
-	arr[l], arr[j] = arr[j], arr[l]
-	return arr
-}
 
 func Factorial(num int) int {
 	if num != 0 {
