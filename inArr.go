658073c62428f804aba9fcb448999497095fb67c
JonnyMurillo288 <39134582+JonnyMurillo288@users.noreply.github.com>
2021-07-06 19:14:34 -0700

    Add files via upload

--- inArr.go
+++ inArr.go
@@ -1,7 +1,5 @@
 package utils
 
-import "fmt"
-
 func InIntArr(arr []int, a int) bool{
 	for _,p := range arr {
 		if p == a {
@@ -24,8 +22,6 @@ func InIntArrArr(all [][]int, arr []int) bool{
 	ind := 0
 	match := len(arr)
 	for _,a := range all {
-		fmt.Println(a)
-		fmt.Println(arr)
 		for _,i := range arr {
 			if InIntArr(a,i) {
 				ind++
