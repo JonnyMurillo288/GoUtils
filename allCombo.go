63819f69d0cf437ad04019686a05447511ec2221
JonnyMurillo288 <39134582+JonnyMurillo288@users.noreply.github.com>
2021-07-06 18:47:10 -0700

    Update allCombo.go

--- allCombo.go
+++ allCombo.go
@@ -13,7 +13,7 @@ func AllCombo(arr []int) [][]int {
 		rand.Seed(int64(time.Now().Second()))
 		j := rand.Intn(len(arr))+1
 		arr[i], arr[j] = arr[j], arr[i]
-		if !InArrArr(all,arr) {
+		if !InIntArrArr(all,arr) {
 			all = append(all,arr)
 		}
 	}
