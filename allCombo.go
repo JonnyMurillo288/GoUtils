f14cc58a881036700883f1cbc9d8115f9c1b5db3
JonnyMurillo288 <39134582+JonnyMurillo288@users.noreply.github.com>
2021-07-07 11:33:37 -0700

    Add files via upload

--- allCombo.go
+++ allCombo.go
@@ -31,6 +31,35 @@ func AllCombo(arr []int) [][]int {
 	return all
 }
 
+
+func Permutations(arr []int)[][]int{
+    var helper func([]int, int)
+    res := [][]int{}
+
+    helper = func(arr []int, n int){
+        if n == 1{
+            tmp := make([]int, len(arr))
+            copy(tmp, arr)
+            res = append(res, tmp)
+        } else {
+            for i := 0; i < n; i++{
+                helper(arr, n - 1)
+                if n % 2 == 1{
+                    tmp := arr[i]
+                    arr[i] = arr[n - 1]
+                    arr[n - 1] = tmp
+                } else {
+                    tmp := arr[0]
+                    arr[0] = arr[n - 1]
+                    arr[n - 1] = tmp
+                }
+            }
+        }
+    }
+    helper(arr, len(arr))
+    return res
+}
+
 func Switch(arr []int, j int, l int) []int {
 	arr[l], arr[j] = arr[j], arr[l]
 	return arr
