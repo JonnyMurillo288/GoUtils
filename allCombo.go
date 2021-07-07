64dee262bd7eae511f9fcbf99b9ca418f654b278
JonnyMurillo288 <39134582+JonnyMurillo288@users.noreply.github.com>
2021-07-06 18:57:31 -0700

    Add files via upload

--- allCombo.go
+++ allCombo.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"math/rand"
 	"time"
-	"fmt"
 )
 
 func AllCombo(arr []int) [][]int {
@@ -11,10 +10,9 @@ func AllCombo(arr []int) [][]int {
 	var all [][]int
 	poss := Factorial(len(arr))
 	for i := 0; i < poss; i++ {
-		rand.Seed(int64(time.Now().Second()))
+		rand.Seed(int64(time.Now().Nanosecond()))
 		j := rand.Intn(len(arr)-1)
 		l := rand.Intn(len(arr)-1)
-		fmt.Println(j,l)
 		arr[l], arr[j] = arr[j], arr[l]
 		if !InIntArrArr(all,arr) {
 			all = append(all,arr)
