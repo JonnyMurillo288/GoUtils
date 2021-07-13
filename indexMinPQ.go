5899fabf17c994c1b6664c946b2fd32f80fad3b8
JonnyMurillo288 <39134582+JonnyMurillo288@users.noreply.github.com>
2021-07-13 13:06:46 -0700

    Update indexMinPQ.go

--- indexMinPQ.go
+++ indexMinPQ.go
@@ -104,6 +104,9 @@ func (i *IndexMinPQ) resize() {
 
 func (i *IndexMinPQ) DelMin() int {
 	N := i.size() - 1 
+	if N == 1 {
+		return -1
+	}
 	log.Println("Deleting min N is:",N)
 	min := i.PQ[0] // first value
 	i.Exch(1,N) // switch the last value with the first
